7dacf9240fc51979148b192c7c92c8f57ea77d04
Garrison <mullenga@tcd.ie>
2023-03-16 13:57:42 +0000

    ci test test

---
+++ tests/inference_test.py
@@ -0,0 +1,10 @@
+import unittest
+# from inference import Inference
+
+
+class TestInference(unittest.TestCase):
+    def test_infer(self, mock_IECore):
+        self.assertEqual("Hello World", "Hello World")
+
+if name == 'main':
+    unittest.main()
