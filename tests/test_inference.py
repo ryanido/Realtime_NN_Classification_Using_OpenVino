78be8c379fc2ceb0c89ce91fb913b82628719c43
MykBit <96201443+mykbit@users.noreply.github.com>
2023-03-22 14:46:49 +0000

    Add ResizeImageLetterbox Testing

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,9 +1,18 @@
 import unittest
-# from inference import Inference
+import numpy as np
+import sys
+sys.path.append("src/")
+from get_inference_input import * 
 
+#Tests for inference helper functions
+class TestResizeImageLetterbox(unittest.TestCase):
+    def setUp(self):
+        self.image = np.zeros((100, 50, 3), dtype=np.uint8)
+        self.size = (200, 200)
 
-class TestInference(unittest.TestCase):
-    def test_infer(self):
-        self.assertEqual("Hello World", "Hello World")
-
-unittest.main()
+    def test_resize_image_letterbox(self):
+        resized_image = resize_image_letterbox(self.image, self.size)
+        self.assertEqual(resized_image.shape[0], self.size[1])
+        self.assertEqual(resized_image.shape[1], self.size[0])  
+if __name__ == '__main__':
+    unittest.main()
