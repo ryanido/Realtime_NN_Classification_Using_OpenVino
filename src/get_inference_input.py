89acc87bbcc7b6c2c643f0d48cc3968eb9c57127
MykBit <96201443+mykbit@users.noreply.github.com>
2023-03-23 13:38:28 +0000

    Add get_inference_input and put_inference_output

--- src/get_inference_input.py
+++ src/get_inference_input.py
@@ -1,37 +1,18 @@
 import cv2
-import numpy as numpy
+import numpy
 
-def resize_image_letterbox(image, size, interpolation=cv2.INTER_LINEAR):
-  ih, iw = image.shape[0:2]
-  w, h = size
-  scale = min(w / iw, h / ih)
-  nw = int(iw * scale)
-  nh = int(ih * scale)
-  dx = (w - nw) // 2
-  dy = (h - nh) // 2
+numpy.set_printoptions(suppress=True, precision=3)
 
-  image = cv2.resize(image, (nw, nh), interpolation=interpolation)
 
-  resized_image = numpy.pad(
-    array           = image, 
-    pad_width       = ((dy, dy + (h - nh) % 2), (dx, dx + (w - nw) % 2), (0, 0)),
-    mode            = 'constant',
-    constant_values = 0
-  )
+def get_inference_input(image, input_size: int, input_name: str):
+  data = cv2.resize(image, (input_size, input_size))
+  data = numpy.array(data)
+  data = numpy.transpose(data, (2, 0, 1))  # hwc -> chw
+  data = data.reshape(1, 3, input_size, input_size)
+  data = data.astype('float32')
 
-  return resized_image
+  input_data = {
+      input_name: data
+  }
 
-def get_inference_input(image, input_size):
-    image_size = [image.shape[0], image.shape[1]]
-    image = resize_image_letterbox(image, [input_size, input_size], 2)
-    data = numpy.array(image)
-    data = numpy.transpose(data, (2, 0, 1)) # hwc -> chw
-    data = data.reshape(1, 3, input_size, input_size) # 3,416,416 -> 1,3,416,416
-    data = data.astype('float32')
-
-    input_data = {
-      'input_1': data,
-      'image_shape': numpy.array([image_size], dtype=numpy.float32)
-    }
-
-    return input_data
+  return input_data
--- src/put_inference_output.py
+++ src/put_inference_output.py
@@ -1,52 +1,66 @@
-import numpy
 import cv2
+import numpy
 
-import lib.coco80_labels as coco
-import lib.colour_palette as palette
+import labels.coco91 as coco
+import colour_palette as palette
+
+numpy.set_printoptions(suppress=True, precision=3)
+
+colours = palette.get_colour_palette(len(coco.labels))
 
 
 def get_most_probable_bounds(box, size):
-  box[0] = max(int(box[0]), 0) # ymin
-  box[1] = max(int(box[1]), 0) # xmin
-  box[2] = min(int(box[2]), size[0]) # ymax
-  box[3] = min(int(box[3]), size[1]) # xmax
+  box[0] = box[0] * size[1]
+  box[1] = box[1] * size[0]
+  box[2] = box[2] * size[1]
+  box[3] = box[3] * size[0]
+  box[0] = max(int(box[0]), 0)
+  box[1] = max(int(box[1]), 0)
+  box[2] = min(int(box[2]), size[1])
+  box[3] = min(int(box[3]), size[0])
 
   return numpy.int32(box)
 
-def put_inference_output(image, inference_output):
-  boxes = inference_output['yolonms_layer_1/ExpandDims_1:0'][0]
-  scores = inference_output['yolonms_layer_1/ExpandDims_3:0'][0]
-  indices = inference_output['yolonms_layer_1/concat_2:0']
 
-  colours = palette.get_colour_palette(len(coco.labels))
-  confidence_threshold = 0.5
+def put_inference_output(image, inference_output):
+  confidence_threshold = 0.3
   label_height = 18
 
-  for index in indices:
-    if (index[0] == -1):
+  for detection in inference_output:
+    score = numpy.float32(detection[2])
+
+    if (score < confidence_threshold):
       break
 
-    score = scores[tuple(index[1:])]
+    box = detection[3:]
+    label_index = numpy.int32(detection[1])
 
     if (score >= confidence_threshold):
-      # ymin, xmin, ymax, xmax
-      box = boxes[index[2]]
       box = get_most_probable_bounds(box, [image.shape[0], image.shape[1]])
-      label_index = index[1]
-      ymin = box[0]
-      xmin = box[1]
-      ymax = box[2]
-      xmax = box[3]
+      xmin = box[0]
+      ymin = box[1]
+      xmax = box[2]
+      ymax = box[3]
 
       det_label = f'{score*100:2.1f}% {coco.labels[label_index]}'
 
       colour = colours[label_index]
       text_colour = (28, 28, 28)
 
-      cv2.rectangle(image, (xmin, ymin), (xmax, ymax), colour, 2)
-      cv2.rectangle(image, (xmin, ymin - label_height), (xmax, ymin),
-                    colour, -1)
-      cv2.rectangle(image, (xmin, ymin - label_height), (xmax, ymin),
-                    colour, 2)
-      cv2.putText(image, det_label, (xmin, ymin - 7),
-                  cv2.FONT_HERSHEY_SIMPLEX, 0.4, text_colour, 1)
+      cv2.rectangle(
+          image,
+          (xmin, ymin), (xmax, ymax),
+          colour, 2)
+      cv2.rectangle(
+          image,
+          (xmin, ymin - label_height), (xmax, ymin),
+          colour, -1)
+      cv2.rectangle(
+          image,
+          (xmin, ymin - label_height), (xmax, ymin),
+          colour, 2)
+      cv2.putText(
+          image,
+          det_label,
+          (xmin, ymin - 7),
+          cv2.FONT_HERSHEY_SIMPLEX, 0.4, text_colour, 1)
