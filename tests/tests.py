eff3055895835c01e4ba6cd96470657a845248eb
murphj88 <124894082+murphj88@users.noreply.github.com>
2023-04-06 13:45:14 +0100

    TestMNGetInferenceInput(unittest.TestCase)

--- tests/tests.py
+++ tests/tests.py
@@ -20,5 +20,24 @@ class TestResizeImageLetterbox(unittest.TestCase):
         self.assertEqual(resized_image.shape[0], self.size[1])
         self.assertEqual(resized_image.shape[1], self.size[0])
 
+        class TestMNGetInferenceInput(unittest.TestCase):
+    def test_mn_get_inference_input(self):
+        # Create a sample image
+        image = np.zeros((128, 128, 3), dtype=np.uint8)
+        image[10:20, 10:20, :] = 255
+
+        # Call the function with some input parameters
+        input_size = 32
+        input_name = 'data'
+        input_data = mn.get_inference_input(image, input_size, input_name)
+
+        # Check that the input data has the expected shape and type
+        self.assertEqual(input_data[input_name].shape, (1, 3, input_size, input_size))
+        self.assertEqual(input_data[input_name].dtype, np.float32)
+
+        # Check that the input data has been correctly preprocessed
+        resized_image = cv2.resize(image, (input_size, input_size))
+        expected_data = np.transpose(resized_image, (2, 0, 1)).reshape(1, 3, input_size, input_size).astype(np.float32)
+        np.testing.assert_allclose(input_data[input_name], expected_data)
 if __name__ == '__main__':
     unittest.main()
